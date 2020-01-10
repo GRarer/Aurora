a6db32fb1481720477e5b6eb8c4b1717c17d1cb9
May Lawver <may@maycod.es>
2020-01-10 14:03:51 -0500

    Add lerp function

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -1,3 +1,7 @@
 export function clamp(min: number, value: number, max: number): number {
     return Math.max(Math.min(value, max), min);
 }
+
+export function lerp(min: number, max: number, t: number): number {
+    return (max - min) * t + min;
+}
