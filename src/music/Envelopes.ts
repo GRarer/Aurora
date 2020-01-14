db2f109c11487b93b3ca87bbdd1ef38c3a407d22
May Lawver <may@maycod.es>
2020-01-13 19:56:46 -0500

    ADSR envelopes can play at different volumes

--- src/music/Envelopes.ts
+++ src/music/Envelopes.ts
@@ -3,22 +3,22 @@ import { lerp } from '../util/Util.js';
 export default class Envelopes {
     
     static createAdsrEnvelope(context: AudioContext, start: number, duration: number, attack: number,
-        decay: number, sustain: number, release: number): GainNode {
+        decay: number, sustain: number, release: number, volume: number = 1): GainNode {
         const gain: GainNode = context.createGain();
         gain.gain.setValueAtTime(0, start);
         if (duration < attack) {
             //note gets cut off in middle of attack
-            gain.gain.linearRampToValueAtTime(lerp(0, 1, duration / attack), start + duration);
+            gain.gain.linearRampToValueAtTime(lerp(0, volume, duration / attack), start + duration);
             gain.gain.linearRampToValueAtTime(0, start + duration + release);
         } else if (duration < attack + decay) {
             //note gets cut off in middle of decay
-            gain.gain.linearRampToValueAtTime(1, start + attack);
-            gain.gain.linearRampToValueAtTime(lerp(1, sustain, (duration - attack)/decay), start + attack + decay);
+            gain.gain.linearRampToValueAtTime(volume, start + attack);
+            gain.gain.linearRampToValueAtTime(lerp(volume, volume * sustain, (duration - attack)/decay), start + attack + decay);
             gain.gain.linearRampToValueAtTime(0, start + duration + release);
         } else {
             //envelope executes fully
-            gain.gain.linearRampToValueAtTime(1, start + attack);
-            gain.gain.linearRampToValueAtTime(sustain, start + attack + decay);
+            gain.gain.linearRampToValueAtTime(volume, start + attack);
+            gain.gain.linearRampToValueAtTime(volume * sustain, start + attack + decay);
             gain.gain.linearRampToValueAtTime(0, start + duration + release);
         }
         return gain;
