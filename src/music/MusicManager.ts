57fcb40f4be711bf45b87fb0706586cd218d1cb3
May Lawver <may@maycod.es>
2020-01-25 15:13:05 -0500

    Procedural subdivisions + drum grooves

--- src/music/MusicManager.ts
+++ src/music/MusicManager.ts
@@ -4,6 +4,7 @@ import { Arrays } from '../util/Arrays.js';
 import Instrument from './Instrument.js';
 import { AdsrOscillatorInstrument } from './Instruments.js';
 import { Random } from '../util/Random.js';
+import Rhythm from './Rhythm.js';
 import { Drumkit, Drums } from './Drums.js';
 
 export default class MusicManager {
@@ -33,6 +34,7 @@ export default class MusicManager {
     }
 
     static masterGain: GainNode;
+    static rhythm: Rhythm;
     static drumkit: Drumkit;
 
     static initialize(): void {
@@ -40,6 +42,7 @@ export default class MusicManager {
         this.masterGain = this.context.createGain();
         this.masterGain.gain.value = 0.25;
         this.masterGain.connect(this.context.destination);
+        this.rhythm = new Rhythm();
         this.drumkit = new Drumkit();
         this.queueNextMeasures(this.context.currentTime, new Chord(60, ChordQualities.MINOR7));
     }
@@ -62,7 +65,8 @@ export default class MusicManager {
     private static queueNextMeasures(startingTime: number, startingChord: Chord): void {
         const beatLength: number = 60 / this.beatsPerMinute;
         let offsetTime: number = 0;
-        const drumLoop: Drums[] = [Drums.KICK, Drums.CLOSED_HI_HAT, Drums.SNARE, Drums.OPEN_HI_HAT];
+        this.rhythm.generateSubdivision(Random.fromArray([5, 6, 7, 8, 9, 11, 13]));
+        const drumLoop: Drums[] = this.generateDrumLoop();
         for (let i = 0; i < drumLoop.length; i++) {
             this.scheduleDrum(startingTime + offsetTime, drumLoop[i]);
             offsetTime += beatLength;
@@ -70,6 +74,31 @@ export default class MusicManager {
         window.setTimeout(() => {
             this.queueNextMeasures(startingTime + offsetTime, startingChord);
         }, (startingTime + offsetTime - this.context.currentTime - 0.2) * 1000);
+    }
+
+    private static generateDrumLoop(): Drums[] {
+        const drumLoop: Drums[] = [];
+        const subdivision: number[] = this.rhythm.subdivision;
+        console.log(this.rhythm.beats, subdivision);
+        let onKick: boolean = true;
+        for (let i = 0; i < subdivision.length; i++) {
+            let acc: number = subdivision[i] - 1;
+            if (onKick) {
+                drumLoop.push(Drums.KICK);
+            } else {
+                drumLoop.push(Drums.SNARE);
+            }
+            onKick = !onKick;
+            while (acc > 0) {
+                if (acc === 1 && i === subdivision.length - 1) {
+                    drumLoop.push(Drums.OPEN_HI_HAT);
+                } else {
+                    drumLoop.push(Drums.CLOSED_HI_HAT);
+                }
+                acc--;
+            }
+        }
+        return drumLoop;
     }
 
     //shift note through the octaves until it ends up between min and max
---
+++ src/music/Rhythm.ts
@@ -0,0 +1,33 @@
+import { Random } from '../util/Random.js';
+
+export default class Rhythm {
+
+    subdivision: number[];
+    _beats: number;
+
+    constructor(beats: number = 8) {
+        this.subdivision = [];
+        this._beats = beats;
+        this.generateSubdivision(beats);
+    }
+
+    get beats(): number {
+        return this._beats;
+    }
+
+    generateSubdivision(beats: number): void {
+        this._beats = beats;
+        this.subdivision = [];
+        const divisions: number[] = [2, 3];
+        const min: number = Math.min(...divisions);
+        while (beats >= min) {
+            const current: number = Random.fromArray(divisions.filter(num => num <= beats));
+            beats -= current;
+            this.subdivision.push(current);
+        }
+        if (beats > 0) {
+            this.subdivision.push(beats);
+        }
+    }
+
+}
