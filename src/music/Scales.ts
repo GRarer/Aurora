514b24b2c11b0e47a1085e3e1f377c077e878190
May Lawver <may@maycod.es>
2020-02-23 11:12:11 -0500

    Started on Scale utilities.

---
+++ src/music/Scales.ts
@@ -0,0 +1,67 @@
+import { mod } from "../util/Util.js";
+
+export type Scale = number; // scales are encoded with numbers in Ian Ring's format
+
+export namespace ScaleUtils {
+
+    export function createScaleFromIntervals(intervals: number[]): Scale {
+        if (!intervals.includes(0)) {
+            throw new Error("scales must have a root");
+        }
+        return intervals.map(interval => 1 << mod(interval, 12)) // mod by 12 to fit into octave
+            .filter((val, i, arr) => arr.indexOf(val) === i) // remove duplicates
+            .reduce((acc, curr) => acc | curr); // combine into final bit vector
+    }
+
+    // turns a scale into the set of pitches from the root
+    // i.e., the scale 2^7 + 2^6 + 2^5 + 2^0 becomes [0, 5, 6, 7]
+    export function getPitchClass(scale: Scale): number[] {
+        const r: number[] = [];
+        let i = 0;
+        while (scale > 0) {
+            if (scale % 1 === 0) {
+                r.push(i);
+            }
+            scale = scale >> 1;
+            i++;
+        }
+        return r;
+    }
+
+    // Rotate a scale left if amount is positive and right if amount is negative
+    export function rotateScale(scale: Scale, amount: number = 1): Scale { // courtesy of nprindle
+        amount = mod(amount, 12); // ensure we only rotate as much as we need to
+        const value = scale << amount | scale >>> (12 - amount);
+        return value & 0xFFF; // mask to make sure it's still a 12-bit vector
+    }
+
+    export function getModes(scale: Scale): Scale[] {
+        const r: Scale[] = [];
+        let curr: Scale = scale;
+        while (curr !== scale) { // once we return to the original scale we're done
+            if (curr & 1) { // only add proper scales - i.e., ones with roots
+                r.push(curr);
+            }
+            curr = rotateScale(curr, 1); // advance to the next mode
+        }
+        return r;
+    }
+
+    export function hasInterval(scale: Scale, semitones: number): boolean {
+        semitones = mod(semitones, 12);
+        return (scale & (1 << semitones)) > 0; // if the semitones-th bit is set, the scale contains a given interval
+    }
+
+    // count the instances of a particular interval in a scale
+    // useful for counting tritonia / hemitonia / etc.
+    export function countIntervals(scale: Scale, semitones: number): number {
+        semitones = mod(semitones, 12);
+        const count: number = getModes(scale) // starting from each note in the scale...
+            .filter(mode => hasInterval(mode, semitones)).length; // count all the notes with a note N semitones above them
+        if (semitones === 6) { // special case (tritones get counted twice since they're symmetrical)
+            return count / 2;
+        }
+        return count;
+    }
+
+}
