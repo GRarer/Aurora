36bd0877f9348e59624f2e6e65a880b6d7658c93
May Lawver <may@maycod.es>
2020-03-07 22:55:02 -0500

    Make arrays.generate arg always defined

--- src/util/Arrays.ts
+++ src/util/Arrays.ts
@@ -8,7 +8,7 @@ export namespace Arrays {
     }
 
     // creates a list of the desired length, filling it with values produced by the given function
-    export function generate<T>(length: number, func: (i?: number) => T): T[] {
+    export function generate<T>(length: number, func: (i: number) => T): T[] {
         const result: T[] = new Array(length);
         for (let i = 0; i < length; i++) {
             result[i] = func(i);
