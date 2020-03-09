244292a6e893b6635de5d62a6bb288f9692f6fcb
May Lawver <may@maycod.es>
2020-03-09 15:26:04 -0400

    Fix constructor style

--- src/music/Instrument.ts
+++ src/music/Instrument.ts
@@ -2,11 +2,7 @@ import { Note } from "./Notes.js";
 
 export default abstract class Instrument {
 
-    volume: number;
-
-    constructor(volume: number) {
-        this.volume = volume;
-    }
+    constructor(public volume: number) {}
 
     abstract scheduleNote(context: AudioContext, note: Note): AudioNode;
 
